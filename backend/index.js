ba7e2b47410b34ae271ac81317ff1e635c1c4e35
ashp79 <112823223+ashp79@users.noreply.github.com>
2024-06-22 00:00:12 +0530

    Second Commit

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
-
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000;
 const cors = require("cors");
-//ispushkarjack0709
-//lRgvQoG3n2J20szy
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 app.use(express.json());
 app.use(cors());
 
 async function main() {
   await mongoose.connect(
-    "mongodb+srv://ispushkarjack0709:lRgvQoG3n2J20szy@delicacies-react-app.bcft0zu.mongodb.net/Delicacies-react-app?retryWrites=true&w=majority&appName=Delicacies-react-app"
+    `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_CLUSTER}/${process.env.MONGODB_DB}?retryWrites=true&w=majority&appName=${process.env.MONGODB_DB}`
   );
 
   app.get("/", (req, res) => {
@@ -24,14 +24,12 @@ main()
   .then(() => console.log("Mongodb Connected Successfully!"))
   .catch((err) => console.log(err));
 
-//routes
+// routes
 const ItemRoutes = require("./src/routes/ItemRoutes.js");
 const CategoryRoutes = require("./src/routes/CategoryRoutes.js");
 
-
-
-app.use('/api',ItemRoutes);
-app.use('/api/',CategoryRoutes);
+app.use('/api', ItemRoutes);
+app.use('/api/', CategoryRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
